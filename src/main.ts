1b12aa2d62f746ce52030789304aae9e6cbc414e
Nathan Chortek <nathanchortek@gmail.com>
2026-09-01 14:24:46 -0400

    slow time step

--- src/main.ts
+++ src/main.ts
@@ -85,7 +85,7 @@ function main() {
 
     // This function will be called every frame
     function tick() {
-        time += 0.2;
+        time += 0.1;
 
         camera.update();
         stats.begin();
